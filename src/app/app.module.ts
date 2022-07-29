595b166572b871862890b502c98b94f1c85c170a
leowipin <leonardopincayjaramillo@gmail.com>
2022-07-29 17:49:44 -0500

    mi primer comentario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SplashComponent } from './splash/splash.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';  
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+const rutas:Routes = [
+  { path: "splash", component: SplashComponent },
+  { path: "principal", component: PrincipalComponent },
+  { path: "**", redirectTo: "splash" }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashComponent,
+    PrincipalComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rutas),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,  
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
